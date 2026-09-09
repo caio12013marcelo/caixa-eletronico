2be973c81670ac8ba5bdbafb4129db49c5286b6e
Cairo <caio12013marcelo@gmail.com>
2026-09-09 19:19:39 -0300

    Update Main.java

--- Caixa/src/Main.java
+++ Caixa/src/Main.java
@@ -4,10 +4,12 @@ public class Main {
     public static void main(String[] args) {
         Usuario u1 = new Usuario();
         CaixaEletronico caixa = new CaixaEletronico();
-        caixa.nota={nota100,}
         Notas nota100 = new Notas();
-        nota100.valor=100;
+        nota100.valor=100,nota20.valor
         nota100.quantNotas=40;
+
+        caixa.nota={nota100};
+
         u1.nome="Carlos";
         u1.saldo=7005;
         Scanner input = new Scanner(System.in);
