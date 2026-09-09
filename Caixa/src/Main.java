101643e5c354064bbb717417ce7a8f149317d7f3
Cairo <caio12013marcelo@gmail.com>
2026-09-08 21:19:03 -0300

    cria projeto

---
+++ Caixa/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Usuario u1 = new Usuario();
+        u1.nome="Carlos";
+        u1.saldo=7005;
+        Scanner input = new Scanner(System.in);
+        System.out.println("======= CAIXA ELETRÔNICO =======\n" +
+                "1 - CONSULTAR SALDO\n" +
+                "2 - SAQUE" +
+                "\n3 - DEPÓSITO" +
+                "\n4 - SAIR" +
+                "\n===============================" +
+                "\nEscolha a opção desejada: ");
+     escolherOpcao(input, u1);
+    }
+    public static void escolherOpcao(Scanner input, Usuario u1){
+        int opc = input.nextInt();
+            switch (opc){
+                case 01:
+                    System.out.println("seu saldo é "+ u1.saldo);
+                    break;
+            }
+    }
+}
