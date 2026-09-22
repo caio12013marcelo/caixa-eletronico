361740e453680a4ca1c02ff5d17eecd311067a20
Cairo <caio12013marcelo@gmail.com>
2026-09-22 19:36:21 -0300

    Update CaixaEletronico.java

--- Caixa/src/CaixaEletronico.java
+++ Caixa/src/CaixaEletronico.java
@@ -7,6 +7,9 @@ public class CaixaEletronico {
 
     public void depositar(int deposito) {
         int resto = deposito;
+        /**
+         * @return ola
+         */
         for (int i = 0; i < notas.length; i++) {
             if (notas[i].valor == 100) {
                 notas[i].quantNotas +=  resto / 100;
